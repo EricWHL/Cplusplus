595ed2b0927bb32b74841de893e4da562e32f9ef
wanghailong <wanghailong@iauto.com>
2020-08-24 12:17:55 +0800

    refresh contents

--- inc/study/Sales_data.h
+++ inc/study/Sales_data.h
@@ -13,14 +13,13 @@ friend std::ostream &print(std::ostream&, const Sales_data&);
 public:
     Sales_data() = default;
 
-    Sales_data(const std::string &s):bookNo(s){}
+    
     
     Sales_data(const std::string &s, unsigned int n, double p):
         bookNo(s), units_sold(n), revenue(p*n) {}
-    
-    Sales_data(std::istream&);
+    explicit Sales_data(const std::string &s):bookNo(s){}
+    explicit Sales_data(std::istream&);
 
-    Sales_data(): Sales_data("", 0, 0) {}
     Sales_data(std::string s): Sales_data(s, 0, 0) {}
 
 
